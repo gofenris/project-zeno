91886e9c89a8e95b35510bd0a1cae9cb167c5e43
Paul Lam <paul@quantisan.com>
2025-05-23 11:52:40 +0900

    init upload_dataset.py

---
+++ experiments/upload_dataset.py
@@ -0,0 +1,24 @@
+import os
+
+from langfuse import Langfuse
+
+langfuse = Langfuse(
+    secret_key=os.getenv("LANGFUSE_SECRET_KEY"),
+    public_key=os.getenv("LANGFUSE_PUBLIC_KEY"),
+    host=os.getenv("LANGFUSE_HOST"),
+)
+
+
+def create_langfuse_dataset(dataset_name):
+    langfuse.create_dataset(name=dataset_name)
+
+
+def insert_langfuse_item(dataset_name, input, expected_output, filename):
+    langfuse.create_dataset_item(
+        dataset_name=dataset_name,
+        # any python object or value, optional
+        input=input,
+        # any python object or value, optional
+        expected_output=expected_output,
+        metadata={"filename": filename},
+    )
